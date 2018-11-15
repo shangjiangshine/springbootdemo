3b2f7d61ec77e05e2d88fb32e1bbfd865243b2ff
ShineYoung <shangjiangshine@163.com>
2018-11-15 18:13:57 +0800

    修改了关于用户的JPA操作

--- src/main/java/com/shineyoung/controller/user/UserController.java
+++ src/main/java/com/shineyoung/controller/user/UserController.java
@@ -2,9 +2,12 @@ package com.shineyoung.controller.user;
 
 import com.shineyoung.dao.user.UserDAO;
 import com.shineyoung.entity.user.UserEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -14,6 +17,8 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
+
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserDAO userDAO;
@@ -48,4 +53,26 @@ public class UserController {
         userDAO.delete(id);
         return userDAO.findAll();
     }
+
+    @RequestMapping(value = "/age")
+    public List<UserEntity> getAgeOlderThan() {
+        return userDAO.getUserOlderThanQuery(18);
+    }
+
+    @RequestMapping(value = "/deleteUser")
+    public String deleteUser(@RequestParam(value = "name") String name,
+                             @RequestParam(value = "pwd") String pwd) {
+        String msg = "删除失败！";
+        try {
+            if(userDAO.existsByNameAndPassword(name, pwd)) {
+                userDAO.deleteQuery(name, pwd);
+                msg = "删除成功！";
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return msg;
+
+    }
+
 }
--- src/main/java/com/shineyoung/dao/user/UserDao.java
+++ src/main/java/com/shineyoung/dao/user/UserDao.java
@@ -3,15 +3,28 @@ package com.shineyoung.dao.user;
 import com.shineyoung.entity.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.JpaRepositoryNameSpaceHandler;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by ShineYoung on 2018/11/13.
  */
-public interface UserDAO extends JpaRepository<UserEntity, Integer>,
-        JpaSpecificationExecutor<UserEntity>,
-        Serializable {
+@Transactional
+public interface UserDAO extends JpaRepository<UserEntity, Integer>, JpaSpecificationExecutor<UserEntity>, Serializable {
 
+        // 查询大于一定岁数的用户
+        @Query(value = "select * from t_user where t_age > ?1", nativeQuery = true)
+        public List<UserEntity> getUserOlderThanQuery(int age);
+
+        //根据用户名、密码删除一条数据
+        @Modifying
+        @Query(value = "delete from t_user where t_name = ?1 and t_password = ?2",nativeQuery = true)
+        public void deleteQuery(String name,String pwd);
+
+        public boolean existsByNameAndPassword(String name, String password);
 }
